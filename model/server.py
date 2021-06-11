852583ada83d000c257364294f9463973ff9d8a6
Hyeon Jeon <hj@hcil.snu.ac.kr>
2021-06-11 15:12:06 +0900

    set server

--- model/server.py
+++ model/server.py
@@ -1,13 +1,51 @@
-from model import MODEL
 
-latent_dims = 5
+
+
+
+
+# def call():
+#     v = MODEL(latent_dims)
+#     vec = np.random.rand(latent_dims)
+#     start_time = time.time()
+#     v.reconstruct(vec)
+#     print("--- %s seconds ---" % (time.time() - start_time))
+
+# call()
+
+
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+
+from model import MODEL
 import numpy as np
 import time
+import json
 
-def call():
-    v = MODEL(latent_dims)
-    vec = np.random.rand(latent_dims)
-    start_time = time.time()
-    v.reconstruct(vec)
-    print("--- %s seconds ---" % (time.time() - start_time))
 
+app = Flask(__name__)
+CORS(app)
+
+latent_dims = 5
+vae = None
+
+
+
+def getArrayData(request, key_name):
+    array_data = request.args.get(key_name)
+    array_data = np.array(json.loads(array_data)["data"]).astype(np.float32)
+    return array_data
+
+@app.route('/reconstruction')
+def reconstruction():
+    latent_values = getArrayData(request, "latentValues")
+
+    return jsonify(vae.reconstruct(np.array(latent_values)).tolist()[0])
+    
+
+
+
+
+if __name__ == '__main__':
+    vae = MODEL(latent_dims)
+    app.run(debug=True)
+    
