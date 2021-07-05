e0344cb5b917eb9edb41a0d59b0d6f77068c4314
Hyeon Jeon <hj@hcil.snu.ac.kr>
2021-07-05 13:46:42 +0900

    umap and tsne embedding generator implemented

--- dataset-generator/embedding.py
+++ dataset-generator/embedding.py
@@ -8,12 +8,15 @@ from sklearn.manifold import LocallyLinearEmbedding
 import numpy as np
 import json
 
-def umap_generate_embeddings(raw_data, path_to_save, iter):
+'''
+UMAP
+'''
+def umap_generate_embeddings(raw_data, path_to_save, iteration, log_interval):
 	umap_path = path_to_save + "umap/"
 	Path(umap_path).mkdir(parents=True, exist_ok=True)
-	for i in range(iter):
+	for i in range(1, iteration + 1):
 		min_dist = np.random.rand()
-		n_neighbors = np.random.randint(0, 100)
+		n_neighbors = np.random.randint(3, 101)
 		emb = umap_embedding(raw_data, min_dist, n_neighbors)
 		with open(umap_path + str(i) + ".json", "w") as outfile:
 			json.dump({
@@ -21,8 +24,9 @@ def umap_generate_embeddings(raw_data, path_to_save, iter):
 				"n_neighbors" : n_neighbors,
 				"emb": emb
 			}, outfile)
-
-
+		if i % log_interval == 0:
+			print("UMAP embedding generation: " + str(i) + "/" + str(iteration) + " finished")
+	print("*** UMAP embedding generation finished. ***")
 
 
 def umap_embedding(raw_data, min_dist, n_neighbors):
@@ -31,8 +35,35 @@ def umap_embedding(raw_data, min_dist, n_neighbors):
 	emb_data = emb_data.tolist()
 	return emb_data
 
-def tsne_embedding(raw_data, perplexity):
-	emb_data = TSNE(n_components=2, metric="euclidean", perplexity=perplexity, random_state=1).fit_transform(raw_data)
+'''
+TSNE
+'''
+def tsne_generate_embeddings(raw_data, path_to_save, iteration, log_interval):
+	tsne_path = path_to_save + "tsne/"
+	Path(tsne_path).mkdir(parents=True, exist_ok=True)
+	for i in range(1, iteration + 1):
+		perplexity = np.random.rand() * 45 + 5
+		early_exaggeration = np.random.rand() * 45 + 5
+		learning_rate = np.random.rand() * 480 + 20
+		emb = tsne_embedding(raw_data, perplexity, early_exaggeration, learning_rate)
+		with open(tsne_path + str(i) + ".json", "w") as outfile:
+			json.dump({
+				"perplexity": perplexity,
+				"early_exaggeration": early_exaggeration,
+				"learning_rate": learning_rate,
+				"emb": emb
+			}, outfile)
+		if i % log_interval == 0:
+			print("t-SNE embedding generation: " + str(i) + "/" + str(iteration) + " finished")
+	print("*** t-SNE embedding generation finished. ***")
+
+
+
+def tsne_embedding(raw_data, perplexity, early_exaggeration, learning_rate):
+	emb_data = TSNE(
+		n_components=2, metric="euclidean", perplexity=perplexity, 
+		early_exaggeration=early_exaggeration, learning_rate=learning_rate, random_state=1
+	).fit_transform(raw_data)
 	emb_data = emb_data.tolist()
 	return emb_data
 
--- dataset-generator/main.py
+++ dataset-generator/main.py
@@ -5,11 +5,15 @@
 #### raw.json, label.json 필요 (각각 원본 데이터 / 레이블 정보를 담고 있는 arry 형태)
 ## -s : 최종적으로 만들어질 embedding의 개수
 ## -p : 임베딩들이 저장될 경로 ("/"로 끝나야 함)
+## -l : 임베딩들을 생성할 때 이 개수만큼의 임베딩을 생성할 때마다 log을 찍어 줌
+
+## 어떤 dataset을 직접 적용해보고 싶으면 맞춰서 json으로 raw, label 파일 다른데서 생성해서 형식 맞춰 디렉토리 만들고 넣어주면 됨
 
 # import os
 # os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 from pathlib import Path
+from multiprocessing import Process
 from embedding import *
 
 import argparse
@@ -21,14 +25,16 @@ parser.add_argument('-d', '--dataset', help="The dataset to generate DR embeddin
 parser.add_argument('-s', '--size',  help="The size of dataset -- name of subdirectory\n" + " (under the directory denoting --dataset)")
 parser.add_argument('-n', '--num', help="The total number of embeddings to be created")
 parser.add_argument('-p', '--path', default="./result/", help="Designate directory for saving generated embeddings")
+parser.add_argument('-l', '--log-interval', default=50, help="print current status per log interver")
 
 
 args = parser.parse_args()
 
-dataset = args.dataset
-size    = args.size
-num     = int(args.num)
-path    = args.path
+dataset       = args.dataset
+size          = args.size
+num           = int(args.num)
+path          = args.path
+log_interval    = int(args.log_interval)
 
 
 ### Read dataset / label
@@ -40,9 +46,10 @@ with open(path_to_dataset + "label.json") as label_file:
 	label = np.array(json.load(label_file))
 
 
-
+## TEMP
 raw = raw[:100]
 label = label[:100]
+
 
 ## Path to save file
 path_to_save = path + "/" + dataset + "/" + size + "/"
@@ -54,8 +61,26 @@ with open(path_to_save + "raw.json", "w") as raw_save_file:
 with open(path_to_save + "label.json", "w") as label_save_file:
 	json.dump(label.tolist(), label_save_file)
 
-## Save umap data
-umap_generate_embeddings(raw, path_to_save, 100)
+print("*** Embedding Generation Start!! ***")
+
+target_list = [
+	umap_generate_embeddings,
+	tsne_generate_embeddings,
+]
+
+iteration = num // len(target_list)
+iteration = 20
+
+process_list = [Process(target=target, args=(raw, path_to_save, iteration, log_interval)) for target in target_list]
+
+for process in process_list:
+	process.start()
+
+for process in process_list:
+	process.join()
+
+
+print("*** Generation Finished. ***")
 
 
 
