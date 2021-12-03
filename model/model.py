79c2298274c1a69f8fbffd4c2f398d25ca9b28c7
aeri206 <aeri33590@gmail.com>
2021-12-03 15:27:49 +0900

    provide file emb

--- model/model.py
+++ model/model.py
@@ -3,48 +3,110 @@ import torch.nn as nn
 import numpy as np
 import math
 from collections import Counter
-
+import json
 
 device= 'cpu'
     
+class VAE(nn.Module):
+    def __init__(self, latent_dims, point_count, hidden_dims: list = None):
+        super(VAE, self).__init__()
+        self.latent_dims = latent_dims
+        self.point_count = point_count
+        self.hidden_dims = hidden_dims
+        en_layers = []
+        in_dim = self.point_count * 2
+        for h_dim in hidden_dims:
+            en_layers.append(
+                nn.Sequential(
+                    nn.Linear(in_dim, h_dim),
+                    nn.ReLU()
+                )
+            )
+            in_dim = h_dim
+        self.encoder = nn.Sequential(*en_layers)
+        self.fc_mu = nn.Linear(hidden_dims[-1], latent_dims)
+        self.fc_sigma = nn.Linear(hidden_dims[-1], latent_dims)
+        
+        de_layers = []
+        hidden_dims = hidden_dims[::-1]
+        in_dim = self.latent_dims
+        for h_dim in hidden_dims:
+            de_layers.append(
+                nn.Sequential(
+                    nn.Linear(in_dim, h_dim),
+                    nn.ReLU()
+                )
+            )
+            in_dim = h_dim
+        de_layers.append(
+            nn.Sequential(
+                nn.Linear(hidden_dims[-1], self.point_count * 2),
+                nn.Sigmoid()
+            )
+        )
+        self.decoder = nn.Sequential(*de_layers)
+        
+    def encode(self, x): 
+        x = self.encoder(torch.flatten(x, start_dim = 1))
+        mu = self.fc_mu(x)
+        sigma = self.fc_sigma(x)
+        return mu, sigma
+    
+    def reparameterize(self, mu, sigma):
+        std = torch.exp(sigma * 0.5)
+        eps = torch.randn_like(std)
+        res = eps * std + mu
+        return res
+    
+    def decode(self, z):
+        z = self.decoder(z)
+        return torch.reshape(z, (-1, self.point_count, 2))
+    
+    def forward(self, x):
+        # x.shape : torch.Size([batch_size, 2, point_count])
+        mu, sigma = self.encode(x)
+        z = self.reparameterize(mu, sigma)
+        return self.decode(z), mu, sigma
+        
+    def get_z(self, x): #x.shape : (1, 2 * point_count)
+        return self.reparameterize(*(self.encode(x)))
+    
+    def get_recon_x(self, z):
+        return self.decode(z)
+        
+    
+
 
 class MODEL:
-    def __init__(self, model_path, dataset, point_num):
+    def __init__(self, model_path, point_num):
         super(MODEL, self).__init__()
-        model_state = torch.load(model_path, map_location=device)
+        
+## 여기부터
+        all_dims = []
+        h_dims = []
+        with open(model_path + 'training_info.json') as f:
+            info = (json.load(f))
+            h_dims = info["hidden_dims"]
+            all_dims = h_dims[:]
+            self.latent_dims = info["latent_dims"]
+            all_dims.append(self.latent_dims)
+            all_dims.append(2 * point_num)
+            all_dims.sort()
+        
+        h_model_path = model_path + 'model.pt'
+        h_model_state = torch.load(h_model_path, map_location=device)
+
+        self.v = VAE(latent_dims = self.latent_dims,
+                    point_count = point_num,
+                    hidden_dims = h_dims).to(device)
+        self.v.load_state_dict(h_model_state, strict=False)
         
         
-        count_dims = Counter(model_state[x].shape[0] for x in model_state.keys() if x.endswith('.bias'))
-        dims = list(set(model_state[x].shape[0] for x in model_state.keys() if x.endswith('.bias')))
-        
-        
-        decoder = []
-        dims.sort()
-        if count_dims[2 * point_num] > 2:
-            dims.append(2 * point_num)
-        self.latent_dims = dims[0]
-        self.relu = nn.ReLU()
-        self.sigmoid = nn.Sigmoid()
-        
-        for i in range(0, len(dims)-1):
-            fc = nn.Linear(dims[i], dims[i+1])
-            ord = str(i+len(dims))
-            fc.weight = nn.Parameter(model_state["fc"+ord+".weight"].to(device))
-            fc.bias = nn.Parameter(model_state["fc"+ord+".bias"].to(device))
-            decoder.append(fc)
-        
-        self.depth = len(decoder) - 1
-        self.decoder = decoder
-        
-        
+    
+    
     def reconstruct(self, latent_vec):
-        
         z = torch.Tensor(latent_vec).view(-1, len(latent_vec)).to(device)
-        for i in range(self.depth):
-            layer = self.decoder[i]
-            z = self.relu(layer(z))
+        x_hat = self.v.get_recon_x(z)
+        return x_hat
+
         
-        layer = self.decoder[self.depth]
-        z = self.sigmoid(layer(z))
-        z = torch.reshape(z, (-1, int(z.shape[1] / 2), 2))
-        return z
--- model/server.py
+++ model/server.py
@@ -18,11 +18,14 @@ vae = None
 latent_emb = None
 latent_label = None #method_num
 latent_vector = None
+files = None
 nn = None
 nn_emb = None
 nn_emb_NUM = 10
 latent_dims = None
 nNeighbors = 50
+dataset = ''
+pointNum = ''
 
 
 
@@ -37,27 +40,39 @@ def reload():
     global latent_emb
     global latent_label
     global latent_vector
+    global files
     global nn
     global nn_emb
     global latent_dims
     global nNeighbors
-    dataset = request.args.get("dataset")
+    global dataset
+    global pointNum
+
+
     pointNum = request.args.get("pointNum")
+    dataset = request.args.get("dataset")
+    idx = request.args.get("idx")
+    print(idx)
     # print(path)
+    tmp_path = './'+ dataset + '/' + pointNum + '/' + idx + '/'
+
     model_dir = './'+ dataset + '/' + pointNum + '/'
-    model_path = model_dir + 'model.pt'
-    vae = MODEL(model_path, dataset, int(pointNum))
+    
+    vae = MODEL(tmp_path, int(pointNum))
     latent_dims = vae.latent_dims
 
-
-    with open(model_dir +'latent_emb.json') as f:
+    print(tmp_path +'latent_emb.json')
+    with open(tmp_path +'latent_emb.json') as f:
         latent_emb = np.array(json.load(f))
         nNeighbors = max(10, min(int(len(latent_emb)* 0.1), 100))
-        print('nNeighbors', nNeighbors)
-    with open(model_dir +'method_num.json') as fi:
+        # print('nNeighbors', nNeighbors)
+    with open(tmp_path +'method_num.json') as fi:
         latent_label = np.array(json.load(fi))
-    with open(model_dir +'latent_vector.json') as fil: # = point_count
+    with open(tmp_path +'latent_vector.json') as fil: # = point_count
         latent_vector = np.array(json.load(fil))
+    with open(tmp_path + 'raw-filename.json') as file:
+        files = np.array(json.load(file))
+
     nn = NearestNeighbors(n_neighbors=nNeighbors, algorithm='ball_tree').fit(latent_vector)
     #latent vector : #embedding * latent_dim
     nn_emb = NearestNeighbors(n_neighbors=nn_emb_NUM, algorithm='ball_tree').fit(latent_emb)
@@ -78,28 +93,55 @@ def get_latent_dims():
 def reconstruction():
     global vae
     latent_values = getArrayData(request, "latentValues")
-    #return shape : (point_count, 2)
-    # print(latent_values)
-
+    
     return jsonify(vae.reconstruct(np.array(latent_values)).tolist()[0])
     
 @app.route('/getlatentemb')
 def get_latent_emb():
     global latent_emb
     global latent_label
+    global latent_vector
+    if latent_emb is None:
+        emb = []
+    else:
+        emb = latent_emb.tolist()
+    if latent_label is None:
+        label = []
+    else:
+        label = latent_label.tolist()
+    if latent_vector is None:
+        vec = []
+    else:
+        vec = latent_vector.tolist()
+
     return jsonify({
-        "emb": latent_emb.tolist(),
-        "label": latent_label.tolist()
+        "emb": emb, 
+        # "emb": latent_emb.tolist(), #에러 나옴
+        "label": label,
+        "vec": vec
     })
+
+def read_file(path):
+    with open(path, 'r') as f:
+        res = json.load(f)
+    return res
+
 
 @app.route('/getknn')
 def get_knn():
     global nn
     global vae
     global latent_label
+    global files
     global latent_emb
     global latent_vector
+    global dataset
+    global pointNum
     n = int(request.args.get("n"))
+
+    # ./mammoth/5000/2/latent_emb.json
+    # './mammoth/5000/2/latent_embs.json'
+    # './mammoth/5000/dr-result/umap-56.json'
     latent_values = getArrayData(request, "latentValues") #내가 요청보낸거
     knn = ((nn.kneighbors([latent_values])[1])[0]).tolist() #top indices
     # print(nn.kneighbors([latent_values])[0])#거리 : (1, 100) : (1, neghbors)
@@ -109,12 +151,18 @@ def get_knn():
         label_result = latent_label[knn]
         latent_result = latent_vector[knn]
         
+        file_result = files[knn]
+        data_dir = './'+ dataset + '/' + pointNum + '/dr-result/' 
+        file_result = [read_file(data_dir + x) for x in file_result]
+        
+        
         x = [vae.reconstruct(x)[0].tolist() for x in latent_result]
         # print(vae.reconstruct(latent_result).shape) #(5, 2000, 2)
         return {
             "labels": label_result.tolist(),
             "embs": x,
-            "latents": latent_result.tolist()
+            "latents": latent_result.tolist(),
+            "files": file_result
         }
     else:
         label_result = latent_label[knn] #class? (neighbors, )
